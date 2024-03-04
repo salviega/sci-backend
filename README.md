e12cc522a533ba082eda3a6451cb8a38d1905726
Santiago Viana <salviega6@gmail.com>
2024-03-03 21:34:48 -0500

    :tada: Start project

---
+++ README.md
@@ -0,0 +1,33 @@
+# Sci Protocol - Backend
+
+BNBChain Hackathon: Building Smart and Fully-On-Chain Applications
+
+## Quick start 🏄
+
+Prerequisites: [Node (v19 LTS)](https://nodejs.org/en/download/) and [Git](https://git-scm.com/downloads)
+
+> Clone the repository:
+
+```
+git clone https://github.com/chrisarevalo11/Sci-frontend.git
+```
+
+> Install dependencies:
+
+```
+yarn
+```
+
+> Run the project:
+
+```
+yarn dev
+```
+
+## Authors 🏗
+
+[chrisarevalo11](https://github.com/chrisarevalo11)
+
+[RookieCol](https://github.com/RookieCol)
+
+[salviega](https://github.com/salviega)
---
+++ src/index.ts
@@ -0,0 +1,181 @@
+import express, { Request, Response, NextFunction } from 'express'
+import multer from 'multer'
+import pinataSDK, { PinataPinOptions } from '@pinata/sdk'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import { validationResult, body } from 'express-validator'
+import fs from 'fs'
+import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors({ origin: '*' }))
+app.use(helmet())
+app.use(morgan('dev'))
+
+const pinataJWTKey = process.env.PINATA_JWT
+const pinata = new pinataSDK({ pinataJWTKey: pinataJWTKey })
+const upload = multer({ dest: 'uploads/' })
+
+// Swagger setup
+const swaggerOptions = {
+	swaggerDefinition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Express IPFS Uploader API',
+			version: '0.0.1',
+			description: 'API for uploading JSON and files to IPFS'
+		},
+		servers: [
+			{
+				url: `http://localhost:${port}`
+			}
+		]
+	},
+	apis: ['./src/**/*.ts'] // Path to the API docs
+}
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+/**
+ * @swagger
+ * /uploadJson:
+ *   post:
+ *     tags:
+ *      - IPFS
+ *     summary: Upload JSON to IPFS
+ *     description: Upload a JSON object to IPFS.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *            type: object
+ *            additionalProperties: true
+ *            description: JSON object to upload to IPFS
+ *            example:
+ *             name: John Doe
+ *             age: 30
+ *             city: New York
+ *             country: USA
+ *
+ *     responses:
+ *       200:
+ *         description: Successfully uploaded JSON to IPFS
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 cid:
+ *                   type: string
+ *                   description: IPFS hash (CID) of the uploaded JSON
+ *       400:
+ *         description: Invalid JSON object
+ *       500:
+ *         description: Error uploading JSON to IPFS
+ */
+
+app.post(
+	'/uploadJson',
+	body().isObject(),
+	async (req: Request, res: Response, next: NextFunction) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		const { body } = req
+		const options: PinataPinOptions = {
+			pinataMetadata: {
+				name: 'test.json'
+			}
+		}
+
+		try {
+			const result = await pinata.pinJSONToIPFS(body, options)
+			res.send(res.send(result.IpfsHash))
+		} catch (error) {
+			console.error(error)
+			next(error)
+		}
+	}
+)
+
+/**
+ * @swagger
+ * /uploadFile:
+ *   post:
+ *     tags:
+ *       - IPFS
+ *     summary: Upload a file to IPFS
+ *     description: Upload a file to IPFS.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *                 description: File to upload to IPFS
+ *     responses:
+ *       200:
+ *         description: Successfully uploaded file to IPFS
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 cid:
+ *                   type: string
+ *                   description: IPFS hash (CID) of the uploaded file
+ *       400:
+ *         description: No file uploaded
+ *       500:
+ *         description: Error uploading file to IPFS
+ */
+
+app.post(
+	'/uploadFile',
+	upload.single('file'),
+	async (req: Request, res: Response, next: NextFunction) => {
+		if (!req.file) {
+			return res.status(400).send('No file uploaded.')
+		}
+
+		const readableStreamForFile = fs.createReadStream(req.file.path)
+		const options: PinataPinOptions = {
+			pinataMetadata: {
+				name: req.file.originalname
+			}
+		}
+
+		try {
+			const result = await pinata.pinFileToIPFS(readableStreamForFile, options)
+			res.send(result.IpfsHash)
+		} catch (error) {
+			console.error(error)
+			next(error)
+		}
+	}
+)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.error(err.stack)
+	res.status(500).send('Something broke!')
+})
+
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`)
+})
